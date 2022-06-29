3d032a7f1a5b605b6321ed88d0b0d5c838223352
Lucas Freitas <lucas@xsys.com.br>
2022-06-29 13:35:31 -0300

    Exercício sobre Importação/Exportação de Módulos.

---
+++ MODULES/main.mjs
@@ -0,0 +1,5 @@
+import { mostraCidade, mostraIdade, mostraHobby } from "./funcoes.mjs";
+
+console.log(mostraIdade("Lucas", 21));
+console.log(mostraCidade("Lucas", "Araras"));
+console.log(mostraHobby("Lucas", "Velejar"));
